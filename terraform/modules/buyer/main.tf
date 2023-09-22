733d732412223c805f7ae69965e5f4c44e4739f9
coffee <coffee@thebitstudio.com>
2023-09-22 14:08:57 +0700

    fix(deployments): use artifact reg vars for images
    Use Artifact Registry Variables for each deployment's image.
    
    This alters based on an assumption that users keep all images in the same artifact registry.

--- terraform/modules/buyer/main.tf
+++ terraform/modules/buyer/main.tf
@@ -542,7 +542,10 @@ resource "kubectl_manifest" "app_gateways" {
 resource "kubectl_manifest" "app_deployments" {
   for_each = fileset(path.module, "manifests/app/deployments/**/*.yaml")
   yaml_body = templatefile("${path.module}/${each.value}", {
-    env_prefix = local.env_prefix
+    env_prefix = local.env_prefix,
+    project    = local.registry_project_id,
+    location   = local.registry_location,
+    repository = local.registry_repository,
   })
   force_conflicts = true
   force_new       = true
